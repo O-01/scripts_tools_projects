740069fcd2c121373393142ab8690c65e7daa526
O-01 <sa.an-s@yandex.com>
2023-12-07 01:43:38 -0600

    28, 29

---
+++ 28-abs.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+
+int absolute_value(int x) {
+    return x < 0 ? x * -1 : x;
+}
+
+
+int main() {
+    int my_num = 12;
+
+    printf("The absolute value of %d is %d.\n", my_num, absolute_value(my_num));
+}
